77e6a05d24446276f4ea1b4926b73f453e2b51fa
Shahnawaz Ansari <absolutez3ro.exe@gmail.com>
2020-08-14 13:40:24 +0530

    Add retry fragment, klaxon dependency and heroes.json file

--- app/src/main/java/com/example/crew/ui/retry/RetryFragment.kt
+++ app/src/main/java/com/example/crew/ui/retry/RetryFragment.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.crew.R
 import com.example.crew.databinding.FragmentRetryBinding
 
 class RetryFragment : Fragment() {
@@ -16,7 +17,8 @@ class RetryFragment : Fragment() {
     ): View? {
         val binding = FragmentRetryBinding.inflate(inflater, container, false)
         binding.retry.setOnClickListener {
-            Toast.makeText(requireContext(), "retry", Toast.LENGTH_SHORT).show()
+            Navigation.findNavController(requireActivity(), R.id.nav_host)
+                .navigate(R.id.action_retry_to_home)
         }
         return binding.root
     }
